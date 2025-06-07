82d16fd5906cb7945f6f893e81172d0285980e4c
caoductin <caoductintin@gmail.com>
2025-06-07 23:39:43 +0700

    feat: add post related view and implement api for post related view

---
+++ AppBlogDATN/View/Home/PostsView/PostRelatedListView.swift
@@ -0,0 +1,18 @@
+//
+//  PostRelatedListView.swift
+//  AppBlogDATN
+//
+//  Created by cao duc tin  on 7/6/25.
+//
+
+import SwiftUI
+
+struct PostRelatedListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostRelatedListView()
+}
