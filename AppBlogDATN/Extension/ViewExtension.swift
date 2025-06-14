b666acaaaea629368d4961c8b1d07abc30bac4a4
caoductin <caoductintin@gmail.com>
2025-06-14 19:27:33 +0700

    feature: implemenmt api CRUD for comment

---
+++ AppBlogDATN/Extension/ViewExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ViewExtension.swift
+//  AppBlogDATN
+//
+//  Created by cao duc tin  on 14/6/25.
+//
+
+import SwiftUI
+
+struct ViewExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewExtension()
+}
---
+++ AppBlogDATN/View/Home/PostsView/ReaderPostView.swift
@@ -0,0 +1,18 @@
+//
+//  ReaderPostView.swift
+//  AppBlogDATN
+//
+//  Created by cao duc tin  on 14/6/25.
+//
+
+import SwiftUI
+
+struct ReaderPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReaderPostView()
+}
